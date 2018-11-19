b9f0c2bbe085d96366f825aaf28789d1624508b3
acuevas <apac.alexcuevas@gmail.com>
2018-11-19 09:59:15 +0100

    repartition en deux dossiers

---
+++ client/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include <QWidget>
+
+int main(int argc, char *argv[])
+{
+    QApplication prog(argc, argv);
+    QWidget window;
+       window.resize(320, 240);
+       window.show();
+       window.setWindowTitle(QApplication::translate("toplevel", "Space Battle"));
+    return prog.exec();
+}
